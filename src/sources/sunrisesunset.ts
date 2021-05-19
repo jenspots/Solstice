9689002177589340ab2f35c986d861ae6b95b8fc
Jens Pots <jenspots@icloud.com>
2021-05-19 22:15:27 +0200

    Skeleton code for www.sunrise-set.org API.

--- src/sources/sunrisesunset.ts
+++ src/sources/sunrisesunset.ts
@@ -1,16 +1,33 @@
-/** Uses the https://sunrise-sunset.org/ API to calculate settings. */
-export class SunsetSunrise implements SettingsSource {
+import fetch from 'node-fetch';
+import {CommandLineArguments, Location} from '../cli';
 
-    private readonly location: Location;
+export interface TimesOfDay {
+    // TODO
+}
+
+/** Uses the https://sunrise-sunset.org/ API to calculate settings. */
+export class SunriseSunset implements SettingsSource {
+
+    /** The requested location described as a Location instance. */
+    private readonly location: Location = {latitude: 1.5643, longitude: 1.456};
 
     constructor(cli: CommandLineArguments) {
         this.location = cli.location;
     }
 
-    async retrieve() : Promise<Response> {
-        return fetch(`https://api.sunrise-sunset.org/json?lat=${location.latitude}&lng=-${location.longitude}&date=today`);
+    private async request() : Promise<TimesOfDay> {
+        // Retrieve data from API.
+        const res = await fetch(`https://api.sunrise-sunset.org/json?lat=${this.location.latitude}&lng=-${this.location.longitude}&date=today&formatted=0`);
+
+        // Interpret as JSON.
+        const json = await res.json(); // TODO: Handle possible errors.
+
+        return {
+            // TODO
+        };
     }
 
+    // TODO
     get(): { brightness: number; temperature: number } {
         return {brightness: 0, temperature: 0};
     }
