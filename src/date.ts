88c2c66ff50ccbe3326f3c2ed3899f0f9e25416f
Jens Pots <jenspots@icloud.com>
2021-05-20 22:57:38 +0200

    Extended the Date prototype with Date::progress.

--- src/date.ts
+++ src/date.ts
@@ -12,8 +12,39 @@ declare global {
     }
 }
 
+declare global {
+    interface Date {
+        /**
+         * Calculate the progress of this Date instance between two other Date objects.
+         * @param start
+         * @param end
+         */
+        progress(start: Date, end: Date) : number;
+    }
+}
+
 Date.fromString = function(ISOString: string) : Date {
     return new Date(Date.parse(ISOString));
+};
+
+Date.prototype.progress = function(start: Date, end: Date) : number {
+    // The end Date instance must be greater than the start Date instance.
+    if (end <= start) {
+        throw Error("Date.prototype.progress: The end date you provided is not greater than the start date.");
+    }
+
+    // If `this` not after start, return 0.
+    if (this <= start) {
+        return 0;
+    }
+
+    // If `this` is not before end, return 1.
+    if (this >= end) {
+        return 1;
+    }
+
+    // Simple calculation that returns the progress.
+    return (this.getTime() - start.getTime()) / (end.getTime() - start.getTime());
 };
 
 export {};
