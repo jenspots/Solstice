565597f2fe75c925e20453229b5620ac24b32417
Jens Pots <jenspots@icloud.com>
2021-05-20 23:55:52 +0200

    Added a singleton class for a Date instance to resemble the current time.

---
+++ src/systemclock.ts
@@ -0,0 +1,19 @@
+/** Singleton pattern of a Date object. By making Solstice rely on this specific Date instance we can easily manipulate
+ * time for testing purpose. Also, this allows the user to choose a "current time" that is different from the real
+ * system clock.*/
+export class SystemClock {
+
+    private static instance: Date;
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    private constructor() {}
+
+    public static getInstance(): Date {
+        if (!SystemClock.instance) {
+            SystemClock.instance = new Date();
+        }
+
+        return SystemClock.instance;
+    }
+
+}
