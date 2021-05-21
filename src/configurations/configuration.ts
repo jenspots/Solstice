b3fc52527927987cb7bfff39818df6904a6fcd9c
Jens Pots <jenspots@icloud.com>
2021-05-21 17:23:14 +0200

    Read JSON files as Configuration instances.

--- src/configurations/configuration.ts
+++ src/configurations/configuration.ts
@@ -1,5 +1,7 @@
 import {State} from "../lights/state";
+import {readFileSync} from 'fs';
 
+/** Indicates how the lights should be set at certain times of day. */
 export interface Configuration {
     sunrise: State;
     sunset: State;
@@ -10,4 +12,14 @@ export interface Configuration {
     civilTwilightEnd: State;
     nauticalTwilightEnd: State;
     astronomicalTwilightEnd: State;
+}
+
+/**
+ * Load a configuration JSON file.
+ * @param url The URL which indicates where the JSON file is located.
+ */
+export function loadConfiguration(url: string) : Configuration {
+    // TODO: Add error handling.
+    const raw = readFileSync(url,'utf8');
+    return JSON.parse(raw) as Configuration;
 }
