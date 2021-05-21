784da008aec434e042d9cb1313ac66d7c8320cd7
Jens Pots <jenspots@icloud.com>
2021-05-21 16:16:45 +0200

    Introduced State interface.

---
+++ src/lights/state.ts
@@ -0,0 +1,6 @@
+/** Instances of this interface model the current or desired state of a light. */
+export interface State {
+    brightness: number,
+    temperature: number,
+    power: boolean,
+}
