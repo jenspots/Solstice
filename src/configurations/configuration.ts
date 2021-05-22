cb9324261fec3aaf28dbc8a686d1b84848450ca2
Jens Pots <jenspots@icloud.com>
2021-05-22 16:56:25 +0200

    Updated the Configuration class with a Configuration::load method.

--- src/configurations/configuration.ts
+++ src/configurations/configuration.ts
@@ -2,7 +2,8 @@ import {State} from "../lights/state";
 import {readFileSync} from 'fs';
 
 /** Indicates how the lights should be set at certain times of day. */
-export interface Configuration {
+export class Configuration {
+
     sunrise: State;
     sunset: State;
     solarNoon: State;
@@ -12,14 +13,33 @@ export interface Configuration {
     civilTwilightEnd: State;
     nauticalTwilightEnd: State;
     astronomicalTwilightEnd: State;
-}
 
-/**
- * Load a configuration JSON file.
- * @param url The URL which indicates where the JSON file is located.
- */
-export function loadConfiguration(url: string) : Configuration {
-    // TODO: Add error handling.
-    const raw = readFileSync(url,'utf8');
-    return JSON.parse(raw) as Configuration;
+    /** This constructor mainly exists to make the TS Compiler happy. */
+    constructor(
+        sunrise: State, sunset: State, solarNoon: State, civilTwilightBegin: State, nauticalTwilightBegin: State,
+        astronomicalTwilightBegin: State, civilTwilightEnd: State, nauticalTwilightEnd: State,
+        astronomicalTwilightEnd: State
+    ) {
+        this.sunrise = sunrise;
+        this.sunset = sunset;
+        this.solarNoon = solarNoon;
+        this.civilTwilightBegin = civilTwilightBegin;
+        this.nauticalTwilightBegin = nauticalTwilightBegin;
+        this.astronomicalTwilightBegin = astronomicalTwilightBegin;
+        this.civilTwilightEnd = civilTwilightEnd;
+        this.nauticalTwilightEnd = nauticalTwilightEnd;
+        this.astronomicalTwilightEnd = astronomicalTwilightEnd;
+    }
+
+    /**
+     * Load a configuration JSON file.
+     * @param url The URL which indicates where the JSON file is located.
+     * @returns A new configuration instance.
+     * @todo Handle errors.
+     */
+    static load(url: string) : Configuration {
+        const raw = readFileSync(url,'utf8');
+        return JSON.parse(raw) as Configuration;
+    }
+
 }
