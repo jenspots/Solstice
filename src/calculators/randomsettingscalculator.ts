17605a741f530f755fe5f8548aac1237f1f58ef7
Jens Pots <jenspots@icloud.com>
2021-06-13 12:25:18 +0200

    Updated SettingsCalculator interface. Will not compile.

--- src/calculators/randomsettingscalculator.ts
+++ src/calculators/randomsettingscalculator.ts
@@ -1,10 +1,12 @@
 import {SettingsCalculator} from "./settingscalculator";
 import {State} from "../lights/state";
+import {Configuration} from "../configurations/configuration";
+import {TimesOfDay} from "../timesofday";
 
 /** Concrete implementation of SettingsCalculator that supplies random values for testing purposes. */
 class RandomSettingsCalculator implements SettingsCalculator {
 
-    async get() : Promise<State> {
+    async get(timesOfDay: TimesOfDay, configuration: Configuration): Promise<State> {
         return {
             brightness: Math.floor(Math.random() * 255),
             temperature: Math.floor(Math.random() * 4500) + 2000,
--- src/calculators/settingscalculator.ts
+++ src/calculators/settingscalculator.ts
@@ -1,6 +1,8 @@
 import {State} from "../lights/state";
+import {Configuration} from "../configurations/configuration";
+import {TimesOfDay} from "../timesofday";
 
 /** Functional interface that defines a method to retrieve the current light settings. */
 export interface SettingsCalculator {
-    get(): Promise<State>;
+    get(timesOfDay: TimesOfDay, configuration: Configuration): Promise<State>;
 }
